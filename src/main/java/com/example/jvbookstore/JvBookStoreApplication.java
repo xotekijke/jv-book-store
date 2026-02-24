20555cf95918aeb46edd935aac632ba4cdef0d51
Xoteki <xotekijke@gmail.com>
2026-02-24 14:25:55 +0300

    spring boot project

---
+++ src/main/java/com/example/jvbookstore/JvBookStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.jvbookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvBookStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JvBookStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jvbookstore/JvBookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jvbookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvBookStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
