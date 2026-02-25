a0c0bface6f261b7aad75f312fe99350a2ae1a57
Xoteki <xotekijke@gmail.com>
2026-02-25 11:35:47 +0300

    issues fixed

--- src/main/java/com/example/jvbookstore/repository/BookRepository.java
+++ src/main/java/com/example/jvbookstore/repository/BookRepository.java
@@ -1,10 +1,8 @@
 package com.example.jvbookstore.repository;
 
 import com.example.jvbookstore.model.Book;
-import org.springframework.stereotype.Repository;
 import java.util.List;
 
-@Repository
 public interface BookRepository {
     Book save(Book book);
 
--- src/main/java/com/example/jvbookstore/repository/BookRepositoryImpl.java
+++ src/main/java/com/example/jvbookstore/repository/BookRepositoryImpl.java
@@ -1,18 +1,48 @@
 package com.example.jvbookstore.repository;
 
-import com.example.jvbookstore.model.Book;
 import java.util.List;
+import com.example.jvbookstore.model.Book;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class BookRepositoryImpl implements BookRepository {
-    private BookRepository bookRepository;
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public BookRepositoryImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
 
     @Override
     public Book save(Book book) {
-        return bookRepository.save(book);
+        Session session;
+        Transaction transaction = null;
+        session = sessionFactory.openSession();
+        try {
+            transaction = session.beginTransaction();
+            session.persist(book);
+            transaction.commit();
+            return book;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't insert user into DB: " + book);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public List<Book> findAll() {
-        return bookRepository.findAll();
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery("FROM Book", Book.class).getResultList();
+        }
     }
 }
