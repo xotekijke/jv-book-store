fcbce6297d8ce812404567a4aca1f0c2386b7aaf
Xoteki <xotekijke@gmail.com>
2026-03-18 12:24:32 +0300

    bookMapperImpl remover

--- src/main/java/com/example/jvbookstore/JvBookStoreApplication.java
+++ src/main/java/com/example/jvbookstore/JvBookStoreApplication.java
@@ -1,14 +1,10 @@
 package com.example.jvbookstore;
 
-import com.example.jvbookstore.service.BookService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JvBookStoreApplication {
-    @Autowired
-    private BookService bookService;
 
     public static void main(String[] args) {
         SpringApplication.run(JvBookStoreApplication.class, args);
--- src/main/java/com/example/jvbookstore/mapper/BookMapper.java
+++ src/main/java/com/example/jvbookstore/mapper/BookMapper.java
@@ -1,9 +1,12 @@
 package com.example.jvbookstore.mapper;
 
+import com.example.jvbookstore.config.MapperConfig;
 import com.example.jvbookstore.dto.BookDto;
 import com.example.jvbookstore.dto.CreateBookRequestDto;
 import com.example.jvbookstore.model.Book;
+import org.mapstruct.Mapper;
 
+@Mapper(config = MapperConfig.class)
 public interface BookMapper {
     BookDto toBookDto(Book book);
 
