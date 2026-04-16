29940a9948bc5fccac402eb86a436e920b982626
Xoteki <xotekijke@gmail.com>
2026-04-16 13:47:36 +0300

    getAllBooks returns Page<T> instead of List<T>

--- src/main/java/com/example/jvbookstore/service/BookService.java
+++ src/main/java/com/example/jvbookstore/service/BookService.java
@@ -2,13 +2,13 @@ package com.example.jvbookstore.service;
 
 import com.example.jvbookstore.dto.BookDto;
 import com.example.jvbookstore.dto.CreateBookRequestDto;
-import java.util.List;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface BookService {
     BookDto save(CreateBookRequestDto requestDto);
 
-    List<BookDto> findAll(Pageable pageable);
+    Page<BookDto> findAll(Pageable pageable);
 
     BookDto getBookById(Long id);
 
