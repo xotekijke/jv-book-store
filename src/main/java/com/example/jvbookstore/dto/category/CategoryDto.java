2c53a6ef671e3288e85599a2227d04902180bfc0
xoteki <xotekijke@gmail.com>
2026-07-24 14:35:29 +0300

    response validation removed

--- src/main/java/com/example/jvbookstore/dto/category/CategoryDto.java
+++ src/main/java/com/example/jvbookstore/dto/category/CategoryDto.java
@@ -1,14 +1,10 @@
 package com.example.jvbookstore.dto.category;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class CategoryDto {
     private Long id;
-
-    @NotBlank
     private String name;
-
     private String description;
 }
